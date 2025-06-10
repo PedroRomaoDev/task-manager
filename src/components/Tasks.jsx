7d24bd4559d6d743458808b15052d781ce049d91
PedroRomaoDev <pedro7ntj@gmail.com>
2025-06-10 18:36:31 -0300

    refactor: add TasksSeparator component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -2,8 +2,9 @@ import Button from "./Button";
 import AddIcon from "../assets/icons/add.svg?react";
 import TrashIcon from "../assets/icons/trash.svg?react";
 import SunIcon from "../assets/icons/sun.svg?react";
-import CloudSunIcon from "../assets/icons/cloud-sun.svg?react";
+import CloudSun from "../assets/icons/cloud-sun.svg?react";
 import MoonIcon from "../assets/icons/moon.svg?react";
+import TasksSeparator from "./TasksSeparator";
 
 const Tasks = () => {
   return (
@@ -16,46 +17,37 @@ const Tasks = () => {
           </span>
           <h2 className="text-xl font-semibold">Minhas Tarefas</h2>
         </div>
+
         <div className="flex items-center gap-3">
           <Button variant="ghost">
-            Limpar Tarefa
+            Limpar tarefas
             <TrashIcon />
           </Button>
+
           <Button>
-            Nova Tarefa
             <AddIcon />
+            Nova tarefa
           </Button>
         </div>
       </div>
 
-      {/* LISTA DE TAREFAS */}
+      {/* MANHÃ */}
       <div className="rounded-xl bg-white p-6">
-        {/* MANHÃ */}
         <div className="space-y-3">
-          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
-            <SunIcon />
-            <p className="text-sm text-[#9A9C9F]">Manhã</p>
-          </div>
+          <TasksSeparator title="Manhã" icon={<SunIcon />} />
         </div>
 
         {/* TARDE */}
         <div className="my-6 space-y-3">
-          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
-            <CloudSunIcon />
-            <p className="text-sm text-[#9A9C9F]">Tarde</p>
-          </div>
+          <TasksSeparator title="Tarde" icon={<CloudSun />} />
         </div>
 
         {/* NOITE */}
         <div className="space-y-3">
-          <div className="flex gap-2 border-b border-solid border-[#F4F4F5] pb-1">
-            <MoonIcon />
-            <p className="text-sm text-[#9A9C9F]">Manhã</p>
-          </div>
+          <TasksSeparator title="Noite" icon={<MoonIcon />} />
         </div>
       </div>
     </div>
   );
 };
-
 export default Tasks;
