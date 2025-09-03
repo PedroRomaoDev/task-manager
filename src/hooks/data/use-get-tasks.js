1e02cb8c76784cc65d6c847a07ebdfe30f2d5b3b
PedroRomaoDev <pedro7ntj@gmail.com>
2025-09-03 12:31:40 -0300

    refactor: add axios

--- src/hooks/data/use-get-tasks.js
+++ src/hooks/data/use-get-tasks.js
@@ -1,14 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 export const useGetTasks = () => {
   return useQuery({
     queryKey: "tasks",
     queryFn: async () => {
       // preciso pegar os dados da API, atualizar o meu state "tasks"
-      const response = await fetch("http://localhost:3000/tasks", {
-        method: "GET",
-      });
-      const tasks = await response.json();
+      const { data: tasks } = await axios.get("http://localhost:3000/tasks");
       return tasks;
     },
   });
